c9471150802bcd486881476dd1ecd3b66f5e481a
Samyeol Son <samyeol0930@gmail.com>
2024-05-27 21:52:18 +0900

    [#8.10] Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,18 @@
-import { cookies } from 'next/headers';
-import { NextRequest } from 'next/server';
-import getSession from './lib/session';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-	const session = await getSession();
-	console.log(session);
-	if (request.nextUrl.pathname === '/profile') {
+	const pathname = request.nextUrl.pathname;
+
+	if (pathname === '/') {
+		const response = NextResponse.next();
+		response.cookies.set('middleware-cookie', 'hello!');
+		return response;
+	}
+	if (pathname === '/profile') {
 		return Response.redirect(new URL('/', request.url));
 	}
 }
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
