2963e2a9947dba789c142242487f8bf4912bc8d3
Samyeol Son <samyeol0930@gmail.com>
2024-06-11 10:00:08 +0900

    [#12.3] Parallel Routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,13 +17,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
+	// @ts-ignore
+	potato,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	console.log(potato);
 	return (
 		<html lang='en'>
 			<body
 				className={`${inter.className} min-h-[100vh] bg-neutral-900 text-white max-w-screen-sm mx-auto`}>
+				{potato}
 				{children}
 			</body>
 		</html>
