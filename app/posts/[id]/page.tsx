4928da315da91234359e9d28f6794d3a694009f8
Samyeol Son <samyeol0930@gmail.com>
2024-06-14 17:03:00 +0900

    [#14.2] Likes and Dislikes

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,9 +1,143 @@
+import { notFound } from "next/navigation";
+import db from "../../../lib/db";
+import Image from "next/image";
+import { formatToTomeAgo } from "../../../lib/utils";
+import { EyeIcon, HandThumbUpIcon } from "@heroicons/react/24/solid";
+import getSession from "../../../lib/session";
+import { revalidatePath } from "next/cache";
+
 type Props = {
-  // props의 타입 정의
+  params: {
+    id: string;
+  };
 };
 
-const page: React.FC<Props> = (props) => {
-  return <div></div>;
+async function getPost(id: number) {
+  try {
+    const post = await db.post.update({
+      where: {
+        id,
+      },
+      data: {
+        views: {
+          increment: 1,
+        },
+      },
+      include: {
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+            likes: true,
+          },
+        },
+      },
+    });
+    return post;
+  } catch (e) {
+    return null;
+  }
+}
+
+async function getIsLiked(postId: number) {
+  const session = await getSession();
+  const like = await db.like.findUnique({
+    where: {
+      id: {
+        postId,
+        userId: session.id!,
+      },
+    },
+  });
+  return Boolean(like);
+}
+
+const PostDetail: React.FC<Props> = async ({ params }) => {
+  const id = +params.id;
+
+  if (isNaN(id)) {
+    return notFound();
+  }
+
+  const post = await getPost(id);
+
+  if (!post) {
+    return notFound();
+  }
+  console.log(post);
+
+  const likePost = async () => {
+    "use server";
+    const session = await getSession();
+    try {
+      await db.like.create({
+        data: {
+          postId: id,
+          userId: session.id!,
+        },
+      });
+      revalidatePath(`/post/${id}`);
+    } catch (e) {}
+  };
+
+  const dislikePost = async () => {
+    "use server";
+    const session = await getSession();
+    try {
+      await db.like.delete({
+        where: {
+          id: {
+            postId: id,
+            userId: session.id!,
+          },
+        },
+      });
+      revalidatePath(`/post/${id}`);
+    } catch (e) {}
+  };
+
+  const isLiked = await getIsLiked(id);
+
+  return (
+    <div className="p-5 text-white">
+      <div className="mb-2 flex items-center gap-2">
+        <Image
+          width={28}
+          height={28}
+          className="size-7 rounded-full"
+          src={post.user.avatar!}
+          alt={post.user.username}
+        />
+        <div>
+          <span className="text-sm font-semibold">{post.user.username}</span>
+          <div className="text-xs">
+            <span>{formatToTomeAgo(post.created_at.toString())}</span>
+          </div>
+        </div>
+      </div>
+      <h2 className="text-lg font-semibold">{post.title}</h2>
+      <p className="mb-5">{post.description}</p>
+      <div className="flex flex-col items-start gap-5">
+        <div className="flex items-center gap-2 text-sm text-neutral-400">
+          <EyeIcon className="size-5" />
+          <span>조회 {post.views}</span>
+        </div>
+        <form action={isLiked ? dislikePost : likePost}>
+          <button
+            className={`flex items-center gap-2 rounded-full border border-neutral-400 p-2 text-sm text-neutral-400 transition-colors hover:bg-neutral-800`}
+          >
+            <HandThumbUpIcon className="size-5" />
+            <span>공감하기 ({post._count.likes})</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default PostDetail;
